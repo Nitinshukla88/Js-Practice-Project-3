a9effb71f516985642e7fd7815aefd67d26204f2
Nitin shukla <nitin.shukla12004@gmail.com>
2024-07-04 00:25:11 +0530

    wrote some js and added a picture

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.getElementById('box').addEventListener("mousemove",function(details){
-    console.log(details.clientX)
-})
+const throttleFunction = (func, delay) => {
+    let prev = 0;
+    return (...args) => {
+        let now = new Date().getTime();
+        if (now - prev > delay) {
+            prev = now;
+            return func(...args);
+        }
+    }
+}
+console.log(document.querySelector('.box'))
+document.querySelector('.box').addEventListener("mousemove",
+    throttleFunction((data) => {
+        let createddiv = document.createElement('div')
+        createddiv.classList.add('photo-holder')
+        let parent = document.querySelector('.box')
+        createddiv.style.left = data.clientX+'px'
+        createddiv.style.top = data.clientY+'px'
+        parent.appendChild(createddiv)
+    }, 500));
