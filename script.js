042153ff9960cc5af993722f8056df1f3cfd42a1
Nitin shukla <nitin.shukla12004@gmail.com>
2024-07-05 23:42:46 +0530

    did some furthur js

--- script.js
+++ script.js
@@ -11,10 +11,14 @@ const throttleFunction = (func, delay) => {
 console.log(document.querySelector('.box'))
 document.querySelector('.box').addEventListener("mousemove",
     throttleFunction((data) => {
-        let createddiv = document.createElement('div')
-        createddiv.classList.add('photo-holder')
+        let element = document.querySelector('.photo-holder')
         let parent = document.querySelector('.box')
-        createddiv.style.left = data.clientX+'px'
-        createddiv.style.top = data.clientY+'px'
-        parent.appendChild(createddiv)
+        // element.style.visibility = "visible"
+        element.style.left = data.clientX+'px'
+        element.style.top = data.clientY+'px'
+        console.log(element)
+        parent.appendChild(element)
+        // setTimeout(() => {
+        //     element.remove()
+        // }, 2000);
     }, 500));
