9f23b8ab1b186fd73bfd56812f350a58a648e863
Nitin shukla <nitin.shukla12004@gmail.com>
2024-07-06 02:26:13 +0530

    Almost finished the Project. This time I wrote js to animate it.

--- script.js
+++ script.js
@@ -11,14 +11,27 @@ const throttleFunction = (func, delay) => {
 console.log(document.querySelector('.box'))
 document.querySelector('.box').addEventListener("mousemove",
     throttleFunction((data) => {
-        let element = document.querySelector('.photo-holder')
+        let newdiv = document.createElement('div')
         let parent = document.querySelector('.box')
-        // element.style.visibility = "visible"
-        element.style.left = data.clientX+'px'
-        element.style.top = data.clientY+'px'
-        console.log(element)
-        parent.appendChild(element)
-        // setTimeout(() => {
-        //     element.remove()
-        // }, 2000);
-    }, 500));
+        newdiv.classList.add('photo-holder')
+        newdiv.style.left = data.clientX-200+'px'
+        newdiv.style.top = data.clientY+'px'
+        let newimg = document.createElement('img')
+        newimg.setAttribute("src","Pic.png")
+        newdiv.appendChild(newimg)
+        parent.appendChild(newdiv)
+        gsap.to(newimg,{
+            y: "0",
+            ease: Power1,
+            duration: .4,
+        })
+        gsap.to(newimg,{
+            y:"100%",
+            delay: .6,
+            ease: Power2,
+            duration: .4
+        })
+        setTimeout(() => {
+            newdiv.remove()
+        }, 2000);
+    }, 300));
